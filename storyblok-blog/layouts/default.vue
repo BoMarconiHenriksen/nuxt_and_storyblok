779e130cad25ff8f5344443b9d4c5be3f8251a09
BoMarconiHenriksen <bomarconi@gmail.com>
2019-08-17 14:41:06 +0200

    Added navigation

--- storyblok-blog/layouts/default.vue
+++ storyblok-blog/layouts/default.vue
@@ -1,14 +1,31 @@
 <template>
   <div>
-    <nuxt />
+    <TheHeader />
+    
+    <main class="main-content">
+      <nuxt />
+    </main>
   </div>
 </template>
+
+<script>
+import TheHeader from '@/components/TheHeader/TheHeader'
+
+export default {
+  components: {
+    TheHeader
+  }  
+}
+</script>
 
 <style>
 * {
   box-sizing: border-box;
   }
-  body {
+body {
     font-family: "Lato", sans-serif;
   }
+.main-content {
+  margin-top: 4.5rem;
+}
 </style>
