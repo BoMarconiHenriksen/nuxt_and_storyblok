f9d1d48ba70fd0454e9d1a2bb335c423627661ee
BoMarconiHenriksen <bomarconi@gmail.com>
2019-08-17 16:11:03 +0200

    Adding content and dynamic routes based on each blog

--- storyblok-blog/pages/index.vue
+++ storyblok-blog/pages/index.vue
@@ -1,16 +1,56 @@
 <template>
-  <div class="container">
-    
-  </div>
+  <section id="posts">
+    <!-- dynamic populate the posts -->
+    <PostPreview 
+      v-for="post in posts"
+      :key="post.id"
+      :title="post.title"
+      :excerpt="post.previewText"
+      :thumbnailImage="post.thumbnailUrl"
+      :id="post.id"
+      />
+  </section>
 </template>
 
 <script>
+import PostPreview from '@/components/Blog/PostPreview';
 
 export default {
-
+  components: {
+    PostPreview
+  },
+  data() {
+    return {
+      posts: [{
+        title: 'A new beginning!',
+        previewText: 'This will be a new beginning don\'t miss it!',
+        thumbnailUrl: 'https://myrepublica.nagariknetwork.com/uploads/media/2019/April/healthy%20fod.jpg',
+        id: 'a-new-beginning'
+       },
+       {
+        title: 'A second beginning!',
+        previewText: 'This will be a second beginning don\'t miss it!',
+        thumbnailUrl: 'https://myrepublica.nagariknetwork.com/uploads/media/2019/April/healthy%20fod.jpg',
+        id: 'a-second-beginning'
+       }
+      ]
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+#posts {
+  padding-top: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column; 
+}
 
+@media (min-width: 35rem) {
+  #posts {
+    flex-direction: row;
+  }
+}
 </style>
